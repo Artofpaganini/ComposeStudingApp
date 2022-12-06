596d91777e1e15cf4e79e07b09a35410c902fb86
Victor Dobrov <VDobrov@alfabank.ru>
2022-12-06 17:17:54 +0300

    feature/lesson2 ShowUserInfo 10 times

--- app/src/main/java/com/viktar/dabrou/composestudingapp/MainActivity.kt
+++ app/src/main/java/com/viktar/dabrou/composestudingapp/MainActivity.kt
@@ -3,12 +3,14 @@ package com.viktar.dabrou.composestudingapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.viktar.dabrou.composestudingapp.ui.theme.ComposeStudingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +20,11 @@ class MainActivity : ComponentActivity() {
             ComposeStudingAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    ShowUserInfo(name = "Anna", age = 27)
+                    Column() {
+                        repeat(10) {
+                            ShowUserInfo(name = "Anna", age = 27)
+                        }
+                    }
                 }
             }
         }
@@ -33,4 +39,12 @@ fun Greeting(name: String) {
 @Composable
 fun ShowUserInfo(name: String, age: Int) {
     Text(text = "Hello $name! You are $age")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeStudingAppTheme {
+        Greeting("Android")
+    }
 }
