17bb82fc98852af8e2069280a185f04b2ad0d5ea
Victor Dobrov <VDobrov@alfabank.ru>
2022-12-06 17:05:42 +0300

    feature/lesson1 ShowUserInfo commit

--- app/src/main/java/com/viktar/dabrou/composestudingapp/MainActivity.kt
+++ app/src/main/java/com/viktar/dabrou/composestudingapp/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.viktar.dabrou.composestudingapp.ui.theme.ComposeStudingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +18,7 @@ class MainActivity : ComponentActivity() {
             ComposeStudingAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    ShowUserInfo(name = "Anna", age = 27)
                 }
             }
         }
@@ -31,10 +30,7 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeStudingAppTheme {
-        Greeting("Android")
-    }
+fun ShowUserInfo(name: String, age: Int) {
+    Text(text = "Hello $name! You are $age")
 }
